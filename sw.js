030f0a3b6e70dec4dfd89af4a816a5bd0852face
jaydeepak.solanki <jaydeepak.solanki@infosys.com>
2020-12-14 13:18:37 +0530

    github pages

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("cacheName").then(function (cache) {
       return cache.addAll([
-        "",
-        "/",
-        "index.html",
-        "manifest.json",
-        "ticTacToe.css",
-        "ticTacToe.js",
+        ".",
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./ticTacToe.css",
+        "./ticTacToe.js",
       ]);
     })
   );
