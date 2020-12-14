12d06b682a996e2c70e30b7fc79b4cd3ef93dd08
jaydeepak.solanki <jaydeepak.solanki@infosys.com>
2020-12-14 13:49:12 +0530

    installable for firebase

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("cacheName").then(function (cache) {
       return cache.addAll([
-        ".",
         "./",
         "./index.html",
         "./lib/manifest.json",
